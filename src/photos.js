a70ac49ed42a5a2c12c08921b4cd1e39d738463c
truongoi <truongoi@yahoo.com>
2020-03-04 15:55:39 -0800

    Added photos + sample images

--- src/photos.js
+++ src/photos.js
@@ -1,47 +1,52 @@
 export const photos = [
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    src: "images/wedding-001.jpg",
     width: 4,
-    height: 3
+    height: 6
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-    width: 1,
-    height: 1
+    src: "images/wedding-002.jpg",
+    width: 4,
+    height: 6
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-    width: 3,
+    src: "images/wedding-003.jpg",
+    width: 4,
+    height: 6
+  },
+  {
+    src: "images/wedding-004.jpg",
+    width: 6,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-    width: 3,
+    src: "images/wedding-005.jpg",
+    width: 6,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-    width: 3,
+    src: "images/wedding-006.jpg",
+    width: 4,
+    height: 6
+  },
+  {
+    src: "images/wedding-007.jpg",
+    width: 6,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-    width: 3,
+    src: "images/wedding-008.jpg",
+    width: 6,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: "images/wedding-009.jpg",
     width: 4,
-    height: 3
+    height: 6
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: "images/wedding-010.jpg",
     width: 4,
-    height: 3
+    height: 6
   }
 ];
